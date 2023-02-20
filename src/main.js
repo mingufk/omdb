f8503a4ba69f582d7d92559784bf9e444722335a
Mingu Kim <mingufk@gmail.com>
2023-02-20 17:25:02 +0900

    ➕ tailwindcss

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import './index.css'
+import "./index.css";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
-    <h1>OMDb</h1>
+    <h1 class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">OMDb</h1>
   </div>
-`
+`;
