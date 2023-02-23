8cbea6a64a43163700eee97fc20ef8878b46e648
Mingu Kim <mingufk@gmail.com>
2023-02-23 23:00:12 +0900

    ✨ Movie list

--- src/store/movie.js
+++ src/store/movie.js
@@ -3,20 +3,38 @@ import Store from "../core/store";
 const store = new Store({
   searchText: "",
   page: 1,
+  maxPage: 1,
   movies: [],
+  loading: false,
+  message: "",
 });
 
 export default store;
 
 export const searchMovies = async (page) => {
+  store.state.loading = true;
+  store.state.page = page;
+
   if (page === 1) {
-    store.state.page = 1;
     store.state.movies = [];
+    store.state.message = "";
   }
 
-  const res = await fetch(
-    `https://www.omdbapi.com/?apikey=7035c60c&s=${store.state.searchText}&page=${page}`
-  );
-  const { Search } = await res.json();
-  store.state.movies = [...store.state.movies, ...Search];
+  try {
+    const res = await fetch(
+      `https://www.omdbapi.com/?apikey=7035c60c&s=${store.state.searchText}&page=${page}`
+    );
+    const { Search, totalResults, Response, Error } = await res.json();
+
+    if (Response === "True") {
+      store.state.movies = [...store.state.movies, ...Search];
+      store.state.maxPage = Math.ceil(Number(totalResults) / 10);
+    } else {
+      store.state.message = Error;
+    }
+  } catch (e) {
+    store.state.message = e.message;
+  } finally {
+    store.state.loading = false;
+  }
 };
