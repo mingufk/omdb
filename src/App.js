f07269018308fb2327ded5d7df2a440491891fe6
Mingu Kim <mingufk@gmail.com>
2023-02-24 23:40:36 +0900

    ✨ Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Component from "./core/component";
+import Header from "./components/Header";
 
 export default class App extends Component {
   render() {
+    const header = new Header().el;
     const routerView = document.createElement("router-view");
 
-    this.el.append(routerView);
+    this.el.append(header, routerView);
   }
 }
--- src/components/Headline.js
+++ src/components/Headline.js
@@ -4,9 +4,9 @@ export default class Headline extends Component {
   render() {
     this.el.classList.add("headline");
     this.el.innerHTML = /*html*/ `
-        <a href="/" class="font-mono text-7xl mb-5  text-amber-500">
+        <h1 href="/" class="font-mono font-black text-7xl mb-5  text-amber-500">
           OMDb
-        </a>
+        </h1>
         <h2 class="text-4xl text-amber-100">The Open Movie Database</h2>
         <p class="opacity-50 my-2.5">
           OMDb (an abbreviation of The Open Movie Database) is an online database of information related to movie, series, episode.
